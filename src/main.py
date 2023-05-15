693f670fd40f477ad0dfb4071546faff60c57000
Cesar Tabares <cesar@maplabs.com>
2023-05-15 16:50:34 -0500

    improvements

--- src/main.py
+++ src/main.py
@@ -3,22 +3,22 @@ from src.locations.factory import LocationFactory
 from src.video_processor import VideoProcessor
 
 
-def main(location_name: str):
+def main(args):
 
-    if not location_name:
+    if not args.location:
         raise ValueError("Location name is required, pass it as --location <location_name>")
 
     factory = LocationFactory()
-    location = factory.create_location(location_name)
+    location = factory.create_location(args.location)
     publisher = "Publisher()"
-    video_processor = VideoProcessor(location, publisher)
+    video_processor = VideoProcessor(location, publisher, debug=args.debug)
     video_processor.start_stream_analysis()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--location', help='location name')
+    parser.add_argument('--debug', help='debug mode')
     args = parser.parse_args()
-    location_name = args.location
-    main(location_name)
+    main(args)
 
